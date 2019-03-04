6afa2e75d03da3a702fb8558da2ecdafd8440ede
Ben Wiklund <ben@daisyowl.com>
2019-03-04 12:43:11 -0800

    helper class

--- spec/flood-fill.spec.ts
+++ spec/flood-fill.spec.ts
@@ -1,27 +1,41 @@
 import { floodFill } from '../src/flood-fill';
 
+class TestCanvas {
+  pixels: number[];
+
+  constructor(public w: number, public h: number) {
+    this.pixels = new Array<number>(this.w * this.h);
+    for (var i = 0; i < this.w * this.h; i++) this.pixels[i] = 0; // array fill not in es5
+  }
+
+  getColor = (x: number, y: number) => this.pixels[x + y * this.w];
+  setColor = (x: number, y: number, color: number) => this.pixels[x + y * this.w] = color;
+  isInBounds = (x: number, y: number) => x >= 0 && y >= 0 && x < this.w && y < this.h;
+  equals = (a: number, b: number) => a == b;
+}
+
 describe("flood fill", () => {
   it("is defined", () => {
     expect(floodFill).toBeDefined();
   });
 
   it("can fill empty image", () => {
-    var w = 5, h = 5;
-    var pixels = new Array<number>(w * h);
-    for (var i = 0; i < w * h; i++) pixels[i] = 0; // array fill not in es5
+    var buffer = new TestCanvas(5, 5);
 
-    var getColor = (x: number, y: number) => pixels[x + y * w];
-    var setColor = (x: number, y: number, color: number) => pixels[x + y * w] = color;
-    var isInBounds = (x: number, y: number) => x >= 0 && y >= 0 && x < w && y < h;
-    var equals = (a: number, b: number) => a == b;
+    expect(buffer.getColor(0, 0)).toEqual(0);
 
-    expect(getColor(0, 0)).toEqual(0);
-
-    floodFill(0, 0, 1, getColor, setColor, isInBounds, equals);
+    floodFill(
+      0, 0,
+      1,
+      buffer.getColor.bind(buffer),
+      buffer.setColor.bind(buffer),
+      buffer.isInBounds.bind(buffer),
+      buffer.equals.bind(buffer)
+    );
 
     var wereAllFilled = true;
-    for (var i = 0; i < w * h; i++) {
-      if (pixels[i] != 1) wereAllFilled = false;
+    for (var i = 0; i < buffer.w * buffer.h; i++) {
+      if (buffer.pixels[i] != 1) wereAllFilled = false;
     }
     expect(wereAllFilled).toEqual(true);
   });
